8c876f02078ed2e07048ab162214ff6b8088355f
Жарков Константин Владимирович <gr424_ghakvl@ttit.local>
2025-02-04 10:42:07 +0700

    title

--- js/main.js
+++ js/main.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#app',
     data: {
         columns: [[], [], []]
+    },
+    methods: {
+        addNote(columnIndex) {
+            let title = prompt("Введите заголовок:");
+            if (title) {
+                this.columns[columnIndex].push({ title });
+            }
+        }
     }
 });
