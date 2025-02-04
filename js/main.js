7b64c46cd2217507530d622942fc2c7711cdf3d0
Жарков Константин Владимирович <gr424_ghakvl@ttit.local>
2025-02-04 11:01:29 +0700

    points

--- js/main.js
+++ js/main.js
@@ -6,9 +6,15 @@ new Vue({
     methods: {
         addNote(columnIndex) {
             let title = prompt("Введите заголовок:");
-            if (title) {
-                this.columns[columnIndex].push({ title });
+            if (!title) return;
+
+            let items = [];
+            for (let i = 0; i < 3; i++) {
+                let task = prompt(`Введите пункт ${i + 1}:`);
+                if (task) items.push({ text: task, done: false });
             }
+
+            this.columns[columnIndex].push({ title, items });
         }
     }
 });
